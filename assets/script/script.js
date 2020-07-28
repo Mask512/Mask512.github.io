24341c45434adc921377ac670990dd3a42f5913a
Mask512 <64330358+Mask512@users.noreply.github.com>
2020-07-29 00:49:26 +0700

    Update script.js

--- assets/script/script.js
+++ assets/script/script.js
@@ -15,3 +15,35 @@ closeProfile.onclick = ()=> {
 myProfile.style.display = "none";    
 }
 
+
+const html = document.getElementById(`html`);
+const css = document.getElementById(`css`);
+const js = document.getElementById(`js`);
+
+html.onclick = ()=> {
+    html.scrollIntoView({ behavior: 'smooth', block: 'end'})
+}
+
+css.onclick = ()=> {
+    css.scrollIntoView({ behavior: 'smooth', block: 'end'})
+}
+
+js.onclick = ()=> {
+    js.scrollIntoView({ behavior: 'smooth', block: 'end'})
+}
+
+const mybutton = document.getElementById("top-btn");
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 250 ) {
+      mybutton.style.display = "block";
+    } else {
+      mybutton.style.display = "none";
+    }
+  }
+
+  function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
